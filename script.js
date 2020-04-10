a69a3fd6f8618da4316837b4c16e7cf837b4a1e2
Elena Kashtanova <lenachestnut@gmail.com>
2020-04-10 17:04:52 +0300

    Add switching between breaks and sessions

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
+// INTERFACE
 const buttons = document.querySelectorAll(".button");
 const timer = document.querySelector(".timer");
 const message = document.querySelector(".message");
 
-let sessionStarted = false;
-let breakStarted = false;
+// TRACKERS
 let intervalStarted = false;
 let sessionCount = 0;
-let paused = false;
+let sessionStarted = false;
+let pausePressed = false;
+let timePassed = 0;
+let timeLeft;
+let interval = null;
 
-const DEFAULT_SESSION = 5;
+// SETTINGS
+const DEFAULT_SESSION = 3;
 const DEFAULT_SHORT = 3;
 const DEFAULT_LONG = 10;
 
@@ -16,10 +21,9 @@ let sessionTime = DEFAULT_SESSION;
 let shortTime = DEFAULT_SHORT;
 let longTime = DEFAULT_LONG;
 
-let timePassed = 0;
-let interval;
-let currentTime;
+let currentTime = DEFAULT_SESSION;
 
+// ON LOAD
 timer.textContent = updateTimer(sessionTime);
 
 buttons.forEach((button) => {
@@ -28,6 +32,21 @@ buttons.forEach((button) => {
         getButton(clickedButton);
     });
 })
+
+function getButton(btn) {
+    if (btn.classList.contains("start")) {
+        if (!intervalStarted) {
+        countDown();
+        displayMessage();
+        }
+    } else if (btn.classList.contains("pause")) {
+        if (timeLeft !== 0) {
+            pauseTimer();
+        }
+    } else if (btn.classList.contains("stop")) {
+        stopTimer();
+    }
+}
 
 function updateTimer(time) {
     let minutes = Math.floor(time / 60);
@@ -44,21 +63,13 @@ function updateTimer(time) {
     return `${minutes}:${seconds}`;
 }
 
-function getButton(btn) {
-    if (btn.classList.contains("start")) {
-        if (!intervalStarted) {
-        countDown();
-        }
-    } else if (btn.classList.contains("pause")) {
-        pauseTimer();
-    } else if (btn.classList.contains("stop")) {
-        stopTimer();
-    }
-}
-
 function countDown() {
 
+    if (!pausePressed) {
     setCurrentTime();
+    }
+
+    pausePressed = false;
 
     interval = setInterval(function() {
         timePassed += 1;
@@ -67,39 +78,54 @@ function countDown() {
 
         if (timeLeft === 0) {
             clearInterval(interval);
+            intervalStarted = false;
+            //toggle session and break
+            sessionStarted = !sessionStarted;
         }
     }, 1000);
 }
 
 function setCurrentTime() {
 
-    currentTime = sessionTime;
+    if (!sessionStarted) {
+        currentTime = sessionTime;
+        timer.textContent = updateTimer(currentTime);
+        timer.classList.remove("break");
+        timePassed = 0;
+    } else if (sessionStarted) {
+        currentTime = shortTime;
+        timer.textContent = updateTimer(currentTime);
+        timer.classList.add("break");
+        timePassed = 0;
+    }
+
     intervalStarted = true;
-    // if (!paused) {
-    //     if (!sessionStarted) {
-    //         currentTime = sessionTime;
-    //         sessionStarted = true;
-    //     } else if (sessionStarted && sessionCount < 4) {
-    //         currentTime = shortTime;
-    //         sessionStarted = false;
-    //     } else if (sessionStarted && sessionCount === 4) {
-    //         currentTime = longTime;
-    //         sessionStarted = false;
-    //     }
-    // } else {
-    //     paused = false;
-    // }
+}
+
+function displayMessage() {
+    if (!sessionStarted) {
+        message.textContent = "Focus!";
+    } else if (sessionStarted) {
+        message.textContent = "Time for a break";
+    }
 }
 
 
 function pauseTimer() {
     clearInterval(interval);
     intervalStarted = false;
+    pausePressed = true;
+    message.textContent = "Paused";
 }
 
 function stopTimer() {
     clearInterval(interval);
+    message.textContent = "Ready?";
     timer.textContent = updateTimer(currentTime);
     timePassed = 0;
     intervalStarted = false;
+
+    if (timeLeft === 0) {
+        sessionStarted = !sessionStarted;
+    }
 }
