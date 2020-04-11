2f3914a84fc67985d2c5bfa100887c9f12290033
Elena Kashtanova <lenachestnut@gmail.com>
2020-04-11 16:56:32 +0300

    add settings functionality

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const buttons = document.querySelectorAll(".button");
 const timer = document.querySelector(".timer");
 const message = document.querySelector(".message");
 const sessionCounter = document.querySelectorAll(".session-count");
+const sessionSetting = document.querySelector(".session-time");
+const shortSetting = document.querySelector(".short-break-time");
+const longSetting = document.querySelector(".long-break-time");
+let popup = null;
 
 // TRACKERS
 let intervalStarted = false;
@@ -10,12 +14,16 @@ let sessionCount = 0;
 let sessionCompleted = false;
 let pausePressed = false;
 let timePassed = 0;
-let timeLeft;
+let timeLeft = null;
 let interval = null;
 
 // SETTINGS
-const DEFAULT_SESSION = 3;
-const DEFAULT_SHORT = 3;
+// const DEFAULT_SESSION = 1500;
+// const DEFAULT_SHORT = 300;
+// const DEFAULT_LONG = 1800;
+
+const DEFAULT_SESSION = 5;
+const DEFAULT_SHORT = 5;
 const DEFAULT_LONG = 10;
 
 let sessionTime = DEFAULT_SESSION;
@@ -23,13 +31,24 @@ let shortTime = DEFAULT_SHORT;
 let longTime = DEFAULT_LONG;
 
 let currentTime = DEFAULT_SESSION;
+let settingCommand;
 
 // ON LOAD
-timer.textContent = updateTimer(sessionTime);
+timer.textContent = formatTime(sessionTime);
+
+function setInitial() {
+    sessionSetting.textContent = formatTime(sessionTime);
+    shortSetting.textContent = formatTime(shortTime);
+    longSetting.textContent = formatTime(longTime);
+}
+
+setInitial();
+
+// TIMER LOGIC
 
 buttons.forEach((button) => {
     button.addEventListener('click', function (e) {
-        clickedButton = e.target;
+        let clickedButton = e.target;
         getButton(clickedButton);
     });
 })
@@ -46,10 +65,29 @@ function getButton(btn) {
         }
     } else if (btn.classList.contains("stop")) {
         stopTimer();
+    } else if (btn.classList.contains("fas")) {
+
+        if (btn.classList.contains("fa-chevron-circle-down")) {
+            settingCommand = "down";
+        } else if (btn.classList.contains("fa-chevron-circle-up")) {
+            settingCommand = "up";
+        }
+
+        if (btn.parentNode.classList.contains("session")) {
+            sessionTime = updateSettings(sessionTime, sessionSetting, btn);
+        } else if (btn.parentNode.classList.contains("short")) {
+            shortTime = updateSettings(shortTime, shortSetting, btn);
+        } else if (btn.parentNode.classList.contains("long")) {
+            longTime = updateSettings(longTime, longSetting, btn);
+        }
+
+        timer.textContent = formatTime(sessionTime);   
+    } else if (btn.classList.contains("reset")) {
+        reset();
     }
 }
 
-function updateTimer(time) {
+function formatTime(time) {
     let minutes = Math.floor(time / 60);
     let seconds = time % 60;
 
@@ -65,7 +103,6 @@ function updateTimer(time) {
 }
 
 function countDown() {
-
     if (!pausePressed) {
     setCurrentTime();
     }
@@ -75,7 +112,7 @@ function countDown() {
     interval = setInterval(function() {
         timePassed += 1;
         timeLeft = currentTime - timePassed;
-        timer.textContent = updateTimer(timeLeft);
+        timer.textContent = formatTime(timeLeft);
 
         if (timeLeft === 0) {
 
@@ -96,37 +133,23 @@ function countDown() {
     }, 1000);
 }
 
-function updateCountDisplay() {
-    let arrCounter = Array.from(sessionCounter);
-
-    arrCounter.forEach((counter) => {
-        counter.classList.remove("complete");
-    })
-
-    for (let i = 0; i < sessionCount; i++) {
-        arrCounter[i].classList.add("complete");
-    }
-}
-
 function setCurrentTime() {
-
     if (!sessionCompleted) {
         currentTime = sessionTime;
-        timer.textContent = updateTimer(currentTime);
+        timer.textContent = formatTime(currentTime);
         timer.classList.remove("break");
         timePassed = 0;
     } else if (sessionCompleted && sessionCount !== 4) {
         currentTime = shortTime;
-        timer.textContent = updateTimer(currentTime);
+        timer.textContent = formatTime(currentTime);
         timer.classList.add("break");
         timePassed = 0;
     } else if (sessionCompleted && sessionCount === 4) {
         currentTime = longTime;
-        timer.textContent = updateTimer(currentTime);
+        timer.textContent = formatTime(currentTime);
         timer.classList.add("break");
         timePassed = 0;
     }
-
     intervalStarted = true;
 }
 
@@ -148,11 +171,76 @@ function pauseTimer() {
 function stopTimer() {
     clearInterval(interval);
     message.textContent = "Ready?";
-    timer.textContent = updateTimer(currentTime);
+    timer.textContent = formatTime(currentTime);
     timePassed = 0;
     intervalStarted = false;
 
     if (timeLeft === 0) {
         sessionCompleted = !sessionCompleted;
+    }
+}
+
+function updateCountDisplay() {
+    let arrCounter = Array.from(sessionCounter);
+
+    arrCounter.forEach((counter) => {
+        counter.classList.remove("complete");
+    })
+
+    for (let i = 0; i < sessionCount; i++) {
+        arrCounter[i].classList.add("complete");
+    }
+}
+
+// SETTINGS
+function updateSettings(time, setting, button) {
+    stopTimer();
+    updateCountDisplay();
+    //select set of setting buttons to keep track of inactive buttons
+    let controls;
+
+    if (setting === sessionSetting) {
+        controls = document.querySelectorAll(".session .button");
+    } else if (setting === shortSetting) {
+        controls = document.querySelectorAll(".short .button");
+    } else if (setting === longSetting) {
+        controls = document.querySelectorAll(".long .button");
+    }
+
+    controls.forEach((c) => {
+        c.classList.remove("inactive");
+    });
+
+    if (time >= 120 || time <= 5340) {
+        if (settingCommand === "down") {
+            if (time > 60) {
+                time-=60;
+            }
+        } else if (settingCommand === "up") {
+            if (time < 5400) {
+                time+=60;
+            }
+        }
+        if (time === 60 || time === 5400) {
+            button.classList.add("inactive");
+        }
+    }
+    setting.textContent = formatTime(time);
+    return time;
+}
+
+function reset() {
+    let agreed = confirm("All progress will be lost. Are you sure?");
+    if (agreed) {
+        sessionTime = DEFAULT_SESSION;
+        shortTime = DEFAULT_SHORT;
+        longTime = DEFAULT_LONG;
+        setInitial();
+        currentTime = sessionTime;
+        timer.classList.remove("break");
+        stopTimer();
+        sessionCount = 0;
+        sessionCompleted = false;
+        updateCountDisplay();
     }
 }
